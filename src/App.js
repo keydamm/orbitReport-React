608d229a6304c1f5f17664833d6315e6ff1befd8
keydamm <keyonamdamm@hotmail.com>
2023-08-10 20:29:32 -0500

    task 1 & 2 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import satData from "./components/satData";
+import { useState } from 'react';
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+export default function Banner() {
+    return (
+        <div>
+            <header>
+                Orbit Reports
+            </header>
+            <p>
+                Click on the buttons to see the satellites in that orbit type
+            </p>
+        </div>
+    );
+}
